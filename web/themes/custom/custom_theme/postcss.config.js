fec7bc1c42db1026e97abc6e1b537c8c939a5988
Rick Hawkins <rlhawk@352283.no-reply.drupal.org>
2021-02-08 14:13:21 -0800

    Update PostCSS configuration

--- web/themes/custom/custom_theme/postcss.config.js
+++ web/themes/custom/custom_theme/postcss.config.js
@@ -1,16 +1,14 @@
 const presetEnv = require('postcss-preset-env')({
   stage: 2,
   features: {
-    'nesting-rules': true
-  }
+    'nesting-rules': true,
+  },
 })
 
 module.exports = {
   plugins: [
     require('tailwindcss')(__dirname + '/tailwind.config.js'),
     presetEnv,
-    ...process.env.NODE_ENV === 'production'
-      ? [require('cssnano')]
-      : []
-  ]
+    ...(process.env.NODE_ENV === 'production' ? [require('cssnano')] : []),
+  ],
 }
